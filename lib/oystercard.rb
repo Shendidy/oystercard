b9b0a23ca672359203402d0e55b53cb3af8443d9
Sherif Shendidy <shendidy@Sherifs-MacBook-Pro.local>
2019-01-14 17:55:07 +0000

    Created a constant with the maximum balance allowed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,14 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :Max_Balance
 
   def initialize(balance = 0)
     @balance = balance
+    @Max_Balance = 90
+  end
+
+  def top_up(amount)
+    (@balance += amount) > @Max_Balance ? raise("Exceeded max balance of #{@Max_Balance}!") : @balance
   end
 
 end
--- spec/units/oystercard_spec.rb
+++ spec/units/oystercard_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe Oystercard do
   it 'check we can start with non default balance' do
     expect(Oystercard.new(20).balance).to eq 20
   end
+
+  it 'responds to top_up' do
+    is_expected.to respond_to(:top_up)
+  end
+
+  it 'is expected to top up balance when required' do
+    oycard = Oystercard.new
+    oycard.top_up(20)
+    expect(oycard.balance).to eq 20
+  end
+
+  it 'raise error when balance exceeds £90' do
+    oycard = Oystercard.new
+    expect {oycard.top_up(100)}.to raise_error("Exceeded max balance of #{oycard.Max_Balance}!")
+  end
 end
