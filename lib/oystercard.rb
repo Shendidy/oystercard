9e876d6ddc7069939140b65d339eeb46da8d623a
Sherif Shendidy <shendidy@Sherifs-MacBook-Pro.local>
2019-01-15 09:53:45 +0000

    Added touch_in and touch_out support

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,11 @@
 class Oystercard
 
-  attr_reader :balance, :Max_Balance
+  attr_reader :balance, :Max_Balance, :in_journey
 
-  def initialize(balance = 0)
+  def initialize(balance = 0, in_journey = false)
     @balance = balance
     @Max_Balance = 90
+    @in_journey = in_journey
   end
 
   def top_up(amount)
@@ -13,6 +14,18 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey = false
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 end
--- spec/units/oystercard_spec.rb
+++ spec/units/oystercard_spec.rb
@@ -1,32 +1,49 @@
 require './lib/oystercard'
 
 RSpec.describe Oystercard do
-  it 'checks that a new card has a balance of 0' do
-    expect(subject.balance).to eq 0
+  context "Initial tests" do
+    it 'checks that a new card has a balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+
+    it 'check we can start with non default balance' do
+      expect(Oystercard.new(20).balance).to eq 20
+    end
+
+    it 'responds to top_up' do
+      is_expected.to respond_to(:top_up)
+    end
+
+    it 'is expected to top up balance when required' do
+      oycard = Oystercard.new
+      oycard.top_up(20)
+      expect(oycard.balance).to eq 20
+    end
+
+    it 'raise error when balance exceeds £90' do
+      oycard = Oystercard.new
+      expect {oycard.top_up(100)}.to raise_error("Exceeded max balance of #{oycard.Max_Balance}!")
+    end
+
+    it 'is expected to deduct jurney rate when instructed to' do
+      oycard = Oystercard.new
+      oycard.top_up(20)
+      expect(oycard.deduct(5)).to eq(15)
+    end
   end
 
-  it 'check we can start with non default balance' do
-    expect(Oystercard.new(20).balance).to eq 20
-  end
+  context "touching in & out starting with £50" do
+    new_card = Oystercard.new(50)
+    it "checking in_journey?" do
+      expect(new_card.in_journey?).to eq false
+    end
 
-  it 'responds to top_up' do
-    is_expected.to respond_to(:top_up)
-  end
+    it "checking touch_in" do
+      expect(new_card.touch_in).to eq true
+    end
 
-  it 'is expected to top up balance when required' do
-    oycard = Oystercard.new
-    oycard.top_up(20)
-    expect(oycard.balance).to eq 20
-  end
-
-  it 'raise error when balance exceeds £90' do
-    oycard = Oystercard.new
-    expect {oycard.top_up(100)}.to raise_error("Exceeded max balance of #{oycard.Max_Balance}!")
-  end
-
-  it 'is expected to deduct jurney rate when instructed to' do
-    oycard = Oystercard.new
-    oycard.top_up(20)
-    expect(oycard.deduct(5)).to eq(15)
+    it "checking touch_out" do
+      expect(new_card.touch_out).to eq false
+    end
   end
 end
